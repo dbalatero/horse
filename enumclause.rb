9a5a7b5930aab2db7532f05780d4e63b589ff1c6
Rob Hanlon <robhanlon@gmail.com>
2010-01-07 17:24:43 -0800

    Now we can add more rules!

--- enumclause.rb
+++ enumclause.rb
@@ -1,16 +1,26 @@
-class Enumclause
-  RULEZ = YAML.load(File.read('rulez.yml'))
-  attr_reader :fortune
+module Horse
+  class Enumclause
+    RULEZ = YAML.load(File.read('rulez.yml'))
+    attr_reader :fortune
 
-  def initialize
-    @fortune = `fortune`.strip
-  end
+    def initialize
+      @fortune = `fortune`.strip
+    end
 
-  def the_rule
-    RULEZ[rand(sum_up_fortune % (RULEZ.size))]
-  end
+    def spit_rule
+      @rule = RULEZ.keys[rand(sum_up_fortune % (RULEZ.size))]
+    end
 
-  def sum_up_fortune
-    @fortune.chars.inject(0) { |m, i| m + i[0] }
+    def rule_violation
+      eval(RULEZ[@rule]).rule_violation
+    end
+
+    def take_valiant_action!(horse_game)
+      eval(RULEZ[@rule]).take_valiant_action!(horse_game)
+    end
+
+    def sum_up_fortune
+      @fortune.chars.inject(0) { |m, i| m + i[0] }
+    end
   end
 end
